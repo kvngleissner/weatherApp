b25f83694d63767899b390c8adbbf51f72bcb1f9
Kevin <kevingleissner@gmx.de>
2022-02-24 17:33:21 +0100

    Added Imports for Background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Sunny from "./assets/Sunny.jpg";
+import Cloudy from "./assets/Cloudy.jpg";
+import Rainy from "./assets/Rainy.jpg";
+import Snowy from "./assets/Snowy.jpg";
+import Overcast from "./assets/Overcast.jpg";
 
 function App() {
-  return <div className="App">
-    
-  </div>;
+  return <div className="App"></div>;
 }
 
 export default App;
